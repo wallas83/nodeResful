3d2b1120343d751afa861f22ae5f53b5d8a68dbd
wilson <whuanca06@gmail.com>
2019-12-31 20:29:31 -0400

    fixing errors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,49 @@
+require('./config/config')
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
-app.get('/', function(req, res) {
-    res.json('Hello world');
+
+//parse application/x-www-form urlencoded
+
+app.use(bodyParser.urlencoded({ extended: false }));
+//parse aplication/json
+
+app.use(bodyParser.json());
+
+
+app.get('/usuario', function(req, res) {
+    res.json('Get Usuario');
+});
+//post crear
+app.post('/usuario', function(req, res) {
+    let persona = req.body;
+    if (persona.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'Nombre necesario'
+        });
+    } else {
+        res.json({
+            persona
+        });
+    }
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando el puerto 3000');
+//put actualiza
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('Post Usuario');
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando el puerto', process.env.PORT);
 });
